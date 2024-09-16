af09393b2e04a6785ebbc1b6df10c8651133ef64
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-09-17 01:19:08 +0200

    I added styles and js

--- static/scroll.js
+++ static/scroll.js
@@ -1,3 +1,59 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const container = document.querySelector('.document-container');
+
+//     function adjustHeight() {
+//         if (container.scrollHeight < 730) {
+//             container.style.height = '730px';
+//         } else {
+//             container.style.height = 'none';
+//         }
+//     }
+
+//     adjustHeight();
+
+//     const observer = new MutationObserver(adjustHeight);
+//     observer.observe(container, { childList: true, subtree: true });
+// });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.document-container');
+
+    function createMessageComponent() {
+        const messageDiv = document.createElement('div');
+        messageDiv.classList.add('message-component');
+        messageDiv.textContent = 'Оберіть pdf-файл, щоб розпочати чаклунство';
+        return messageDiv;
+    }
+
+    function removeMessageComponent() {
+        const messageDiv = container.querySelector('.message-component');
+        if (messageDiv) {
+            messageDiv.remove();
+        }
+    }
+
+    function adjustHeight() {
+        if (container.scrollHeight < 730) {
+            container.style.height = '730px';
+            container.style.display = 'flex';
+            container.style.justifyContent = 'center';
+            container.style.alignItems = 'center';
+            if (!container.querySelector('.message-component')) {
+                container.appendChild(createMessageComponent());
+            }
+        } else {
+            container.style.height = 'none';
+            removeMessageComponent();
+        }
+    }
+
+    adjustHeight();
+
+    const observer = new MutationObserver(adjustHeight);
+    observer.observe(container, { childList: true, subtree: true });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     function scrollToBottom() {
         const container = document.querySelector('.document-container-two');
